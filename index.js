9815ad951c58238b4d51a2190ea30b68f28fc3aa
Nikolina Šušak <nsusak0@gmail.com>
2021-04-25 10:28:54 +0200

    added some basic styling, middlewares and product crud

--- index.js
+++ index.js
@@ -2,13 +2,16 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cookieSession = require("cookie-session");
 const authRouter = require("./routes/admin/auth");
+const productsRouter = require("./routes/admin/products");
 const app = express();
 
+app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieSession({
     keys: ["lksmcarnjlkmscf"]
 }));
 app.use(authRouter);
+app.use(productsRouter);
 
 app.listen(3000, () => {
     console.log("App is listening on port 3000")
--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { check, validationResult } = require("express-validator");
+const { handleErrors } = require("./middlewares");
 const usersRepo = require("../../repositories/users");
 const signupTemplate = require("../../views/admin/auth/signup");
 const signinTemplate = require("../../views/admin/auth/signin");
@@ -16,15 +16,12 @@ router.post("/signup",
         requirePassword,
         requirePasswordConfirmation
     ],
+    handleErrors(signupTemplate),
     async (req, res) => {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return res.send(signupTemplate({ req, errors }))
-        }
-        const { email, password, passwordConfirmation } = req.body;
+        const { email, password } = req.body;
         const user = await usersRepo.create({ email, password });
         req.session.userId = user.id;
-        res.send("Account created!!")
+        res.redirect("/admin/products");
     });
 
 router.get("/signout", (req, res) => {
@@ -36,15 +33,15 @@ router.get("/signin", (req, res) => {
     res.send(signinTemplate({}));
 });
 
-router.post("/signin", [requireEmailExists, requireValidPasswordForUser], async (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.send(signinTemplate({ errors }));
-    }
-    const { email } = req.body;
-    const user = await usersRepo.getOneBy({ email });
-    req.session.userId = user.id;
-    res.send("You are signed in")
-});
+router.post(
+    "/signin",
+    [requireEmailExists, requireValidPasswordForUser],
+    handleErrors(signinTemplate),
+    async (req, res) => {
+        const { email } = req.body;
+        const user = await usersRepo.getOneBy({ email });
+        req.session.userId = user.id;
+        res.redirect("/admin/products");
+    });
 
 module.exports = router;
